7cc437928e4586c8c6ce7597b2bdc47733122731
Aleksi1212 <aleksinoro@gmail.com>
2024-12-05 15:30:42 +0200

    UPDATE_PLAYER_DATA and UPDATE_PLAYER_VISITED queries

--- queries.py
+++ queries.py
@@ -9,9 +9,24 @@ GET_GAMES = """
 """
 
 GET_CURRENT_PLAYER_LOCATION_DATA = """
-    SELECT airport.ident, airport.name, airport.latitude_deg, airport.longitude_deg, player.money
+    SELECT airport.ident, airport.name, airport.latitude_deg, airport.longitude_deg, airport.continent, player.money
     FROM airport
     JOIN player
     ON airport.ident = player.location
     WHERE player.Id = %s
+"""
+
+UPDATE_PLAYER_DATA = """
+    UPDATE player
+    SET location = %s, co2_consumed = co2_consumed + %s, money = money + %s
+    WHERE Id = %s
+"""
+
+UPDATE_PLAYER_VISITED = """
+    INSERT INTO player_visited (player_id, visited_continent)
+    SELECT %s, %s
+    WHERE NOT EXISTS (
+        SELECT 1 FROM player_visited
+        WHERE player_id = %s AND visited_continent = %s
+    )
 """
