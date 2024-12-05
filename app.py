6f2bcbffa782ee8a03979f6c99efdb0c715492a6
Aleksi1212 <aleksinoro@gmail.com>
2024-12-05 15:29:53 +0200

    get continent from db

--- app.py
+++ app.py
@@ -83,12 +83,13 @@ def game():
         'name': current_airport[1],
         'latitude_deg': current_airport[2],
         'longitude_deg': current_airport[3],
+        'continent': current_airport[4],
         'cost_of_flight': 0,
         'co2_consumtion': 0,
         'flyable': False
     }
 
-    cursor.execute('SELECT ident, name, latitude_deg, longitude_deg FROM airport')
+    cursor.execute('SELECT ident, name, latitude_deg, longitude_deg, continent FROM airport')
     all_airports = cursor.fetchall()
 
     flyable_airports = get_flyable_airports(current_airport, all_airports)
@@ -97,7 +98,7 @@ def game():
     page_data = {
         'airports': flyable_airports,
         'current_airport': current_airport_json,
-        'access_token_data': access_token_data
+        'access_token_data': access_token_data,
     }
 
     return render_template('game.html', data=page_data)
