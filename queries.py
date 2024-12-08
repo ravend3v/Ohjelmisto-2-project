fb88d668e78eba00549eed6d72695a039f4378cb
ravend3v <ojalaelias05@gmail.com>
2024-12-08 17:51:10 +0200

    done

--- queries.py
+++ queries.py
@@ -8,6 +8,16 @@ GET_GAMES = """
     GROUP BY pg.game_id;
 """
 
+GET_GAME_BY_ID = """
+    SELECT pg.game_id, 
+        GROUP_CONCAT(p.user_name ORDER BY p.user_name ASC SEPARATOR ',  ') AS players
+    FROM player_game pg
+    JOIN player p ON pg.player_id = p.Id
+    JOIN game g ON pg.game_id = g.Id
+    WHERE g.game_over = 0 AND pg.game_id = %s
+    GROUP BY pg.game_id;
+"""
+
 GET_CURRENT_PLAYER_LOCATION_DATA = """
     SELECT airport.ident, airport.name, airport.latitude_deg, airport.longitude_deg, airport.continent, player.money
     FROM airport
