f343491c7760d126098f3772c2d61313b6f10e3f
Aleksi1212 <aleksinoro@gmail.com>
2024-12-09 16:56:48 +0200

    refactor queries and make game work with new database

--- queries.py
+++ queries.py
@@ -1,6 +1,5 @@
 GET_GAMES = """
-    SELECT pg.game_id, 
-        GROUP_CONCAT(p.user_name ORDER BY p.user_name ASC SEPARATOR ',  ') AS players
+    SELECT pg.game_id, g.location, p.user_name
     FROM player_game pg
     JOIN player p ON pg.player_id = p.Id
     JOIN game g ON pg.game_id = g.Id
@@ -8,42 +7,26 @@ GET_GAMES = """
     GROUP BY pg.game_id;
 """
 
-UPDATE_PLAYER_LOCATION = """
-    UPDATE player
-    SET location = %s
-    WHERE Id = %s;
-"""
-
-GET_GAME_BY_ID = """
-    SELECT pg.game_id, 
-        GROUP_CONCAT(p.user_name ORDER BY p.user_name ASC SEPARATOR ',  ') AS players
-    FROM player_game pg
-    JOIN player p ON pg.player_id = p.Id
-    JOIN game g ON pg.game_id = g.Id
-    WHERE g.game_over = 0 AND pg.game_id = %s
-    GROUP BY pg.game_id;
-"""
-
-GET_CURRENT_PLAYER_LOCATION_DATA = """
-    SELECT airport.ident, airport.name, airport.latitude_deg, airport.longitude_deg, airport.continent, player.money
+GET_GAMES_CURRENT_LOCATION_DATA = """
+    SELECT airport.ident, airport.name, airport.latitude_deg, airport.longitude_deg, airport.continent, game.money
     FROM airport
-    JOIN player
-    ON airport.ident = player.location
-    WHERE player.Id = %s
+    JOIN game
+    ON airport.ident = game.location
+    WHERE game.Id = %s
 """
 
-UPDATE_PLAYER_DATA = """
-    UPDATE player
+UPDATE_GAME_DATA = """
+    UPDATE game
     SET location = %s, co2_consumed = co2_consumed + %s, money = money + %s
     WHERE Id = %s
 """
 
 UPDATE_PLAYER_VISITED = """
-    INSERT INTO player_visited (player_id, visited_continent)
+    INSERT INTO game_visited (game_id, visited_continent)
     SELECT %s, %s
     WHERE NOT EXISTS (
-        SELECT 1 FROM player_visited
-        WHERE player_id = %s AND visited_continent = %s
+        SELECT 1 FROM game_visited
+        WHERE game_id = %s AND visited_continent = %s
     )
 """
 
@@ -54,7 +37,8 @@ CREATE_PLAYER_BASE_STATS = """
 """
 
 CREATE_GAME = """
-    INSERT INTO game (game_over) VALUES (0)
+    INSERT INTO game (game_over, money, co2_consumed, location)
+    VALUES (0, %s, %s, %s)
 """
 
 CREATE_PLAYER_GAME = """
